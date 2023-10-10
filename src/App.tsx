3660a7ff29a715fa3112b39af3a36a58cd92c0d5
Mukesh Patel <mukesh.patel@fdmgroup.com>
2023-10-10 10:02:34 +0100

    Skeleton app created

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
