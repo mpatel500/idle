97343ce3eaf61515d7179477600222ce948b99bd
mpatel500 <mpatel125@outlook.com>
2023-10-11 20:00:01 +0100

    Complete v1 of inventory layout@

--- src/PartyTab/PartyTab.tsx
+++ src/PartyTab/PartyTab.tsx
@@ -2,39 +2,40 @@ import React from 'react';
 
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const PartyTab = () => {
   return (
     <Box>
       <Box>
-        Party Party!
+      <Typography variant='h6'>Inventory</Typography>
       </Box>
-      <Grid container spacing={0} justifyContent='center' direction='row'>
-        <Grid item xs={4} sx={{p:0}}>
+      <Grid container justifyContent='center' spacing={2} sx={{width:200}}>
+        <Grid item>
           <Box component='img' sx={{width: 50, height: 50}}/>
         </Grid>
-        <Grid item xs={4} sx={{p:0}}>
+        <Grid item>
           <Box component='img' sx={{width: 50, height: 50}} />
         </Grid>
-        <Grid item xs={4} sx={{p:0}}>
+        <Grid item> 
           <Box component='img' sx={{width: 50, height: 50}} />
         </Grid>
-        <Grid item xs={4} sx={{p:0, px:0}}>
-          <Box component='img' sx={{px:0, width: 50, height: 50}} />
-        </Grid>
-        <Grid item xs={4}>
+        <Grid item>
           <Box component='img' sx={{width: 50, height: 50}} />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item>
           <Box component='img' sx={{width: 50, height: 50}} />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item>
           <Box component='img' sx={{width: 50, height: 50}} />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item>
           <Box component='img' sx={{width: 50, height: 50}} />
         </Grid>
-        <Grid item xs={4}>
+        <Grid item>
+          <Box component='img' sx={{width: 50, height: 50}} />
+        </Grid>
+        <Grid item>
           <Box component='img' sx={{width: 50, height: 50}} />
         </Grid>
       </Grid>
