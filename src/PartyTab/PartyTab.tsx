5966bca46598d5305bdcf52484a5b0246b0b31d4
mpatel500 <mpatel125@outlook.com>
2023-11-09 15:59:25 +0000

    Add draggable placeholder inventory row

--- src/PartyTab/PartyTab.tsx
+++ src/PartyTab/PartyTab.tsx
@@ -1,57 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import { Droppable } from 'react-beautiful-dnd';
+import { Draggable } from 'react-beautiful-dnd';
+
 import EquippedArmour from '../EquippedArmour';
 
-const PartyTab = () => {
+const PartyTab = ({ items }: { items: number[] }) => {
   return (
     <Box>
-			<Grid container justifyContent={'center'}>
-				<Grid item xs={4}>
-					<Box>
-						<Typography variant='h6'>Equipped</Typography>
-					</Box>
-					<EquippedArmour />
-				</Grid>
-				<Grid item xs={8}>
-					<Typography variant='h6'>Stats</Typography>
-					<List dense={false}>
-						<ListItem>
-							Bigstat: Very big stat
-						</ListItem>
-					</List>
-				</Grid>
-				<Grid item xs={12}>
-					<Typography variant='h6'>Inventory</Typography>
-					<InventoryRow />
-					<InventoryRow />
-					<InventoryRow />
-				</Grid>
-			</Grid>
+      <Grid container justifyContent={'center'}>
+        <Grid item xs={4}>
+          <Box>
+            <Typography variant='h6'>Equipped</Typography>
+          </Box>
+          <EquippedArmour />
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant='h6'>Stats</Typography>
+          <List dense={false}>
+            <ListItem>
+              Bigstat: Very big stat
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item xs={12} justifyContent='center'>
+          <Typography variant='h6' sx={{py: 3}}>Inventory</Typography>
+					<InventoryRow id={1} items={items}/>
+        </Grid>
+      </Grid>
     </Box>
-  )
-}
+  );
+};
 
-const InventoryRow = () => (
-	<Box sx={{py: 0.5}}>
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-		<Box component='img' sx={{ width: 50, height: 50, px: 1 }} />
-	</Box>
-)
-
+const InventoryRow = ({ id, items }: { id: number, items: number[] }) => {
+	return (
+		<Droppable droppableId={`grid-${id}`} direction="horizontal">
+			{(provided) => (
+				<Grid
+					container
+					spacing={2}
+					ref={provided.innerRef}
+					{...provided.droppableProps}
+					justifyContent='center'
+				>
+					{items.map((item, index) => (
+						<Draggable
+							key={item.toString()}
+							draggableId={item.toString()}
+							index={index}
+						>
+							{(provided, snapshot) => (
+								<Grid
+									item
+									xs={1}
+									ref={provided.innerRef}
+									{...provided.draggableProps}
+									{...provided.dragHandleProps}
+									style={{
+										background: snapshot.isDragging ? 'grey' : 'lightgrey',
+										...provided.draggableProps.style,
+									}}
+								>
+									<Box
+										component='img'
+										src={`https://placekitten.com/50/50?image=${item}`} 
+										alt={`item-${item.toString()}`}
+									/>
+								</Grid>
+							)}
+						</Draggable>
+					))}
+					{provided.placeholder}
+				</Grid>
+			)}
+		</Droppable>
+	)
+};
 
 export default PartyTab;
--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import PartyTab from '../PartyTab';
 
@@ -14,9 +15,9 @@ interface TabPanelProps {
 
 const TabPanel = ({children, index, value}: TabPanelProps) => (
   <div
-    role='tabpanel'
-    id={`tabpanel-${index}`}
-    style={{width: '100%'}}
+  role='tabpanel'
+  id={`tabpanel-${index}`}
+  style={{width: '100%'}}
   >
     {value === index && (
       <Box sx={{p: 3}}>
@@ -27,28 +28,51 @@ const TabPanel = ({children, index, value}: TabPanelProps) => (
 )
 
 const Sidebar = () => {
-  // starting at 1 because technically the heading is index 0
-  const [value, setValue] = useState(1)
+  // starting at 1 because the heading is index 0
+  const [value, setValue] = useState(1);
+  const [isInitialRender, setIsInitialRender] = useState(true);
+  const [partyMemberItems, setPartyMemberItems] = useState(
+    [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  );
 
+  function onDragEnd(result: any) {
+    console.log('onDragEnd');
+    if (!result.destination) {
+      return;
+    }
+  
+    const newItems = Array.from(partyMemberItems);
+    const [movedItem] = newItems.splice(result.source.index, 1);
+    newItems.splice(result.destination.index, 0, movedItem);
+  
+    setPartyMemberItems(newItems);
+  }
+
+  useEffect(() => {
+    if (isInitialRender) {
+      setIsInitialRender(false);
+    }
+  }, [isInitialRender])
+  
   return (
-    <Box
-      sx={{ flexGrow: 1, display: 'flex', p:2}}
-    >
-      <Tabs
-        orientation='vertical'
-        value={value}
-        onChange={(e: React.SyntheticEvent, index: number) => setValue(index)}
-      >
-        <Typography variant='h6' sx={{p: 2, pt: 1}}>Final Fantasy Idle</Typography>
-        <Tab label='Party' />
-        <Tab label='Combat' />
-        <Tab label='Placeholder' />
-      </Tabs>
-      <TabPanel value={value} index={1}>
-        <PartyTab />
-      </TabPanel>
+    <Box sx={{ flexGrow: 1, display: 'flex', p: 2 }}>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Tabs
+          orientation='vertical'
+          value={value}
+          onChange={(e: React.SyntheticEvent, index: number) => setValue(index)}
+        >
+          <Typography variant='h6' sx={{ p: 2, pt: 1 }}>Final Fantasy Idle</Typography>
+          <Tab label='Party' />
+          <Tab label='Combat' />
+          <Tab label='Placeholder' />
+        </Tabs>
+        <TabPanel value={value} index={1}>
+          {!isInitialRender && value === 1 && <PartyTab items={partyMemberItems}/>}
+        </TabPanel>
+      </DragDropContext>
     </Box>
-  )
-}
+  );
+};
 
 export default Sidebar;
